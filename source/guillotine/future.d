e15cad58db7b1a8da758cd72ba7ba5d2f3acd577
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-22 08:57:04 +0200

    Future
    - Implemented `complete(Result)` which sets this `Future` as completed by storing the provided result into it and waking up anybody who was awaiting it

--- source/guillotine/future.d
+++ source/guillotine/future.d
@@ -43,4 +43,27 @@ public final class Future
     {
         this.event = event;
     }
+
+    /** 
+     * Sets this `Future` as completed by storing the
+     * provided result into it and waking up anybody
+     * who was awaiting it
+     *
+     * Params:
+     *   result = the task's result
+     *
+     * Throws:
+     *   TODO: Handle the libsnooze event
+     */
+    package void complete(Result result)
+    {
+        // Store the result
+        this.result = result;
+
+        // Set the state
+        this.state = State.FINISHED;
+
+        // Wake up any sleepers
+        this.event.notifyAll();
+    }
 }
