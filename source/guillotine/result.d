bcfc00d2c16bd9d5cdca2000e15f5ef4ed34c118
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-25 20:55:23 +0200

    Result
    - Documented module, field, constructor and both methods

--- source/guillotine/result.d
+++ source/guillotine/result.d
@@ -1,3 +1,7 @@
+/** 
+ * Defines a result which is retruned
+ * from the process of awaiting a future
+ */
 module guillotine.result;
 
 import guillotine.value : Value;
@@ -9,18 +13,39 @@ import guillotine.value : Value;
  */
 public struct Result
 {
+    /** 
+     * The value of the result
+     */
     private Value value;
 
+    /** 
+     * Constructs a new `Result` with
+     * the given value
+     *
+     * Params:
+     *   value = the `Value` of this result
+     */
     this(Value value)
     {
         this.value = value;
     }
 
+    /** 
+     * Returns the value of this result
+     *
+     * Returns: the `Value`
+     */
     public Value getValue()
     {
         return value;
     }
 
+    /** 
+     * Sets this result's value
+     *
+     * Params:
+     *   value = the `value` to set to
+     */
     public void setValue(Value value)
     {
         this.value = value;
